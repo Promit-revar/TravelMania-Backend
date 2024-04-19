a40eaaf54b09095661e27979fc67e22f6165b564
Promit <promit.revar@takeda.com>
2024-04-19 16:11:36 +0530

    fix: cors issue

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 const router = require("./routes/web");
 const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(cors({origin:'*'}));
+
 const port = process.env.PORT || 8000;
 app.listen(port,()=>console.log(`server running on port: ${port}`));
