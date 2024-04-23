81561cf694ea7549a10b4f71eb42701850326794
Promit <promit.revar@takeda.com>
2024-04-24 00:47:42 +0530

    feat: add error handling functionality

--- routes/web.js
+++ routes/web.js
@@ -13,30 +13,60 @@ router.post('/hotel_search',async (req,res)=>{
       };
     const url = BASE_URL+"hotel_search";
     const response = await makeRequest({method:'POST',url:url, body:{...payload}});
-    res.json(response);
+    if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error')){
+      res.status(500).json({success: false, error: 'Something went wrong'});
+    }
+    else if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'errors')){
+      res.status(400).json({sucess: false, error:response.status.errors})
+    }
+    else{
+      res.status(200).json(response);
+    }
 });
 router.get('/hotelDetails',async (req,res)=>{
   const params = {...req.query};
   const url = BASE_URL+"hotelDetails";
   const response = await makeRequest({method: 'GET', url: url, params:{...params}});
-  res.json(response);
+  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error')){
+    res.status(500).json({success: false, error: 'Something went wrong'});
+  }
+  else if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'errors')){
+    res.status(400).json({sucess: false, error:response.status.errors})
+  }
+  else{
+    res.status(200).json(response);
+  }
 });
 router.get('/moreResults', async (req,res)=>{
   const params = {...req.query};
   const url = BASE_URL+"moreResults";
   const response = await makeRequest({method: 'GET', url: url, params:{...params}});
-  res.json(response);
+  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error')){
+    res.status(500).json({success: false, error: 'Something went wrong'});
+  }
+  else if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'errors')){
+    res.status(400).json({sucess: false, error:response.status.errors})
+  }
+  else{
+    res.status(200).json(response);
+  }
 });
 router.post('/filterResults', async (req,res) => {
   const payload = {...req.body}
   const url = BASE_URL+"filterResults";
   const response = await makeRequest({method: 'POST', url, body:{...payload}});
-  res.json(response);
+  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error'))
+    res.json({success: false, error: response.status.error});
+  else
+    res.json(response);
 });
 router.get('/moreFilterResults', async (req,res) => {
   const params = {...req.query};
   const url = BASE_URL+"moreFilterResults?";
   const response = await makeRequest({method: 'GET', url: url, params:{...params}});
-  res.json(response);
+  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error'))
+    res.json({success: false, error: response.status.error});
+  else
+    res.json(response);
 })
 module.exports = router;
