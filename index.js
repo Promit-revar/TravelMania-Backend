fbf38538b80f359c892ec99fd25a11cbdd5875f6
Promit <promit.revar@takeda.com>
2024-05-18 01:04:33 +0530

    fix: webhook

--- index.js
+++ index.js
@@ -16,24 +16,44 @@ app.post('/api/webhook',express.raw({ type: 'application/json' }),async(request,
       response.status(400).json({success: false, error: `Webhook Error: ${err.message}`});
       return;
     }
-    // Handle the event
     try{
+    // Handle the event
+    console.log(event.type);
     switch (event.type) {
-      case 'payment_intent.requires_action':
-        const paymentIntentRequiresAction = event.data.object;
-        console.log(paymentIntentRequiresAction);
-        // Then define and call a function to handle the event payment_intent.requires_action
+      case 'checkout.session.async_payment_succeeded':
+        const checkoutSessionAsyncPaymentSucceeded = event.data.object;
+        // Then define and call a function to handle the event checkout.session.async_payment_succeeded
+        await sendEmail();
+        break;
+      case 'checkout.session.completed':
+        const checkoutSessionCompleted = event.data.object;
+        // Then define and call a function to handle the event checkout.session.completed
         break;
       case 'payment_intent.succeeded':
         const paymentIntentSucceeded = event.data.object;
-        console.log(paymentIntentSucceeded);
-        await sendEmail();
         // Then define and call a function to handle the event payment_intent.succeeded
         break;
       // ... handle other event types
       default:
         console.log(`Unhandled event type ${event.type}`);
     }
+ 
+    // switch (event.type) {
+    //   case 'payment_intent.requires_action':
+    //     const paymentIntentRequiresAction = event.data.object;
+    //     console.log(paymentIntentRequiresAction);
+    //     // Then define and call a function to handle the event payment_intent.requires_action
+    //     break;
+    //   case 'payment_intent.succeeded':
+    //     const paymentIntentSucceeded = event.data.object;
+    //     console.log(paymentIntentSucceeded);
+    //     await sendEmail();
+    //     // Then define and call a function to handle the event payment_intent.succeeded
+    //     break;
+    //   // ... handle other event types
+    //   default:
+    //     console.log(`Unhandled event type ${event.type}`);
+    // }
   }
   catch(err){
     response.status(500).json({success: false, error: err.message});
