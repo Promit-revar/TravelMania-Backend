eb0d96ddba8e5eb02c9d6f5bc6bd78d1ef5129c2
Promit <promit.revar@takeda.com>
2024-05-18 00:53:27 +0530

    feat: integrated webhooks for stripe

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const sendEmail = require('./utils/sendMail');
 const app = express();
 app.use(cors());
 app.post('/api/webhook',express.raw({ type: 'application/json' }),async(request, response) => {
-    // const sig = request.headers['stripe-signature'];
+    const sig = request.headers['stripe-signature'];
     let event;
-    // const endpointSecret = "whsec_31a291a60be2ed797c8d6884a27848f0dc828be87ef1279482c4ee83a077c810";
-    // try {
-    //   event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    // } catch (err) {
-    //   response.status(400).json({success: false, error: `Webhook Error: ${err.message}`});
-    //   return;
-    // }
+    const endpointSecret = "whsec_31a291a60be2ed797c8d6884a27848f0dc828be87ef1279482c4ee83a077c810";
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).json({success: false, error: `Webhook Error: ${err.message}`});
+      return;
+    }
     // Handle the event
     try{
     switch (event.type) {
