02c4cc687638dd2521fcc9d0ac8e3980f82f02dd
Promit <promit.revar@takeda.com>
2024-06-01 16:51:54 +0530

    feat: fixed issues

--- routes/web.js
+++ routes/web.js
@@ -1,122 +1,195 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 const BASE_URL = "https://travelnext.works/api/hotel_trawexv6/";
-const makeRequest = require('../utils/makeRequest');
-const createStripeSession = require('../utils/stripeConfig');
-router.get('/',(req,res)=>{
-    res.send("Hello World!");
-})
-router.post('/hotel_search',async (req,res)=>{
-    const payload = {
-      "user_id": process.env.USER_NAME,
-      "user_password": process.env.USER_PASSWORD,
-      "ip_address": process.env.IP_ADDRESS,
-      "maxResult": Number.MAX_SAFE_INTEGER,
-       ...req.body
-      };
-    const url = BASE_URL+"hotel_search";
-    const response = await makeRequest({method:'POST',url:url, body:{...payload}});
-    if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error')){
-      res.status(500).json({success: false, error: 'Something went wrong'});
-    }
-    else if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'errors')){
-      res.status(400).json({sucess: false, error:response.status.errors})
-    }
-    else{
-      res.status(200).json(response);
-    }
+const makeRequest = require("../utils/makeRequest");
+const createStripeSession = require("../utils/stripeConfig");
+router.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-router.get('/hotelDetails',async (req,res)=>{
-  const params = {...req.query};
-  const url = BASE_URL+"hotelDetails";
-  const response = await makeRequest({method: 'GET', url: url, params:{...params}});
-  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error')){
-    res.status(500).json({success: false, error: 'Something went wrong'});
-  }
-  else if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'errors')){
-    res.status(400).json({sucess: false, error:response.status.errors})
-  }
-  else{
+router.post("/hotel_search", async (req, res) => {
+  const payload = {
+    user_id: process.env.USER_NAME,
+    user_password: process.env.USER_PASSWORD,
+    ip_address: process.env.IP_ADDRESS,
+    maxResult: Number.MAX_SAFE_INTEGER,
+    ...req.body,
+  };
+  const url = BASE_URL + "hotel_search";
+  const response = await makeRequest({
+    method: "POST",
+    url: url,
+    body: { ...payload },
+  });
+  if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "error")
+  ) {
+    res.status(500).json({ success: false, error: "Something went wrong" });
+  } else if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "errors")
+  ) {
+    res.status(400).json({ sucess: false, error: response.status.errors });
+  } else {
     res.status(200).json(response);
   }
 });
-router.get('/moreResults', async (req,res)=>{
-  const params = {...req.query};
-  const url = BASE_URL+"moreResults";
-  const response = await makeRequest({method: 'GET', url: url, params:{...params}});
-  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error')){
-    res.status(500).json({success: false, error: 'Something went wrong'});
-  }
-  else if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'errors')){
-    res.status(400).json({sucess: false, error:response.status.errors})
-  }
-  else{
+router.post("/store", (req,res)=>{
+    const payload = {...req.body};
+    const token = jwt.sign(
+      {
+        ...payload,
+      },
+      process.env.JWT_SECRET
+    );
+    res.status(200).json({success: true, token })
+});
+router.get("/hotelDetails", async (req, res) => {
+  const params = { ...req.query };
+  const url = BASE_URL + "hotelDetails";
+  const response = await makeRequest({
+    method: "GET",
+    url: url,
+    params: { ...params },
+  });
+  if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "error")
+  ) {
+    res.status(500).json({ success: false, error: "Something went wrong" });
+  } else if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "errors")
+  ) {
+    res.status(400).json({ sucess: false, error: response.status.errors });
+  } else {
     res.status(200).json(response);
   }
 });
-router.post('/filterResults', async (req,res) => {
-  const payload = {...req.body}
-  const url = BASE_URL+"filterResults";
-  const response = await makeRequest({method: 'POST', url, body:{...payload}});
-  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error'))
-    res.json({success: false, error: response.status.error});
-  else
-    res.json(response);
-});
-router.get('/moreFilterResults', async (req,res) => {
-  const params = {...req.query};
-  const url = BASE_URL+"moreFilterResults?";
-  const response = await makeRequest({method: 'GET', url: url, params:{...params}});
-  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error'))
-    res.json({success: false, error: response.status.error});
-  else
-    res.json(response);
-});
-router.post('/get_room_rates', async (req,res) => {
-  const payload = {...req.body};
-  const url = BASE_URL+"get_room_rates";
-  const response = await makeRequest({method: 'POST', url: url, body: {...payload}});
-  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error'))
-    res.json({success: false, error: response.status.error});
-  else if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'errors')){
-      res.status(400).json({sucess: false, error:response.status.errors})
-  }
-  else
-    res.json(response);
-});
-router.post('/get_rate_rules', async(req,res)=>{
-  const payload = {...req.body};
-  const url = BASE_URL+"get_rate_rules";
-  const response = await makeRequest({method: 'POST', url: url, body: {...payload}});
-  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error'))
-    res.json({success: false, error: response.status.error});
-  else if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'errors')){
-      res.status(400).json({sucess: false, error:response.status.errors})
-  }
-  else
+router.get("/moreResults", async (req, res) => {
+  const params = { ...req.query };
+  const url = BASE_URL + "moreResults";
+  const response = await makeRequest({
+    method: "GET",
+    url: url,
+    params: { ...params },
+  });
+  if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "error")
+  ) {
+    res.status(500).json({ success: false, error: "Something went wrong" });
+  } else if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "errors")
+  ) {
+    res.status(400).json({ sucess: false, error: response.status.errors });
+  } else {
     res.status(200).json(response);
-});
-router.post('/booking', async(req,res)=>{
-  try{
-  const payload = {...req.body};
-  const url = BASE_URL+"hotel_book";
-  const response = await makeRequest({method: 'POST', url: url, body: {...payload}});
-  if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'error'))
-    res.json({success: false, error: response.status.error});
-  else if(Object.hasOwn(response,'status') && Object.hasOwn(response.status,'errors')){
-      res.status(400).json({sucess: false, error:response.status.errors})
   }
-  else{
-    const stripeSession = await createStripeSession({
-      price: response.roomBookDetails.NetPrice,
-      currency: response.roomBookDetails.currency,
-      data: response.roomBookDetails,
+});
+router.post("/filterResults", async (req, res) => {
+  const payload = { ...req.body };
+  const url = BASE_URL + "filterResults";
+  const response = await makeRequest({
+    method: "POST",
+    url,
+    body: { ...payload },
+  });
+  if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "error")
+  )
+    res.json({ success: false, error: response.status.error });
+  else res.json(response);
+});
+router.get("/moreFilterResults", async (req, res) => {
+  const params = { ...req.query };
+  const url = BASE_URL + "moreFilterResults?";
+  const response = await makeRequest({
+    method: "GET",
+    url: url,
+    params: { ...params },
+  });
+  if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "error")
+  )
+    res.json({ success: false, error: response.status.error });
+  else res.json(response);
+});
+router.post("/get_room_rates", async (req, res) => {
+  const payload = { ...req.body };
+  const url = BASE_URL + "get_room_rates";
+  const response = await makeRequest({
+    method: "POST",
+    url: url,
+    body: { ...payload },
+  });
+  if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "error")
+  ){
+    res.json({ success: false, error: response.status.error });
+  }
+  else if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "errors")
+  ) {
+    res.json({ sucess: false, error: response.status.errors });
+  } else res.status(200).json(response);
+});
+router.post("/get_rate_rules", async (req, res) => {
+  const payload = { ...req.body };
+  const url = BASE_URL + "get_rate_rules";
+  const response = await makeRequest({
+    method: "POST",
+    url: url,
+    body: { ...payload },
+  });
+  if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "error")
+  )
+    res.json({ success: false, error: response.status.error });
+  else if (
+    Object.hasOwn(response, "status") &&
+    Object.hasOwn(response.status, "errors")
+  ) {
+    res.json({ sucess: false, error: response.status.errors });
+  } else res.status(200).json(response);
+});
+router.post("/booking", async (req, res) => {
+  try {
+    const payload = { ...req.body.requestData };
+    const url = BASE_URL + "hotel_book";
+    const response = await makeRequest({
+      method: "POST",
+      url: url,
+      body: { ...payload },
     });
-    res.status(200).json({bookingData: response, stripeSession});
+    if (
+      Object.hasOwn(response, "status") &&
+      Object.hasOwn(response.status, "error")
+    )
+      res.json({ success: false, error: response.status.error });
+    else if (
+      Object.hasOwn(response, "status") &&
+      Object.hasOwn(response.status, "errors")
+    ) {
+      res.status(400).json({ sucess: false, error: response.status.errors });
+    } else {
+      // console.log(req.body)
+      const stripeSession = await createStripeSession({
+        price: response.roomBookDetails.NetPrice,
+        currency: response.roomBookDetails.currency,
+        data: {checkIn: response.roomBookDetails.checkIn, checkOut: response.roomBookDetails.checkOut, refNo: response.referenceNum, cancellationPolicy: response.roomBookDetails.cancellationPolic, rooms: response.roomBookDetails.rooms, email: response.roomBookDetails.customerEmail, lat: req.body.geoData.lat, long: req.body.geodata.long},
+      });
+      res.status(200).json({ bookingData: response, stripeSession });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, error: "Something went wrong" });
   }
-}catch(err){
-  console.log(err);
-  res.status(500).json({success: false, error: 'Something went wrong'})
-}
-    
 });
 module.exports = router;
--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 module.exports = emailTemplate = ({email, name, bookingData}) => ({
     from: `"Phuket-Concierge" < ${process.env.ADMIN_EMAIL_ID}>`, // sender address
-    to: `${email}, ${process.env.ADMIN_EMAIL_ID}`, // list of receivers
+    to: `${bookingData.email}, ${process.env.ADMIN_EMAIL_ID}`, // list of receivers
     subject: "Your Booking has been confirmed ✔", // Subject line
     text: "Hello world?", // plain text body
     html: `<b>Greetings</b>&nbsp; Mr. ${name},<br><br>
@@ -9,16 +9,14 @@ module.exports = emailTemplate = ({email, name, bookingData}) => ({
             <ul>
                 <li> <b>Check-in : ${bookingData.checkIn} </b> </li>
                 <li> <b>Check-out : ${bookingData.checkOut} </b> </li>
-                <li><b> Cancellation Policy: ${bookingData.cancellationPolicy}</b></li>
             </ul>
             <h2> Room Details </h2>
             <ul>
-                <li> <b>Room Type : ${bookingData.rooms[0].name} </b> </li>
-                <li> <b>Number of Guests : ${bookingData.rooms[0].paxDetails.name.length} </b> </li>
-            </ul>
-            <h2>Booking For</h2>
-            <ul>
-            ${bookingData.rooms[0].paxDetails.name.map(guest => `<li><b>${guest}</b></li>`)}
-            </ul>
+                <li> <b>Room Types : ${bookingData.rooms[0].name} </b> </li>
+                <li><b> Cancellation Policy: ${bookingData.cancellationPolicy}</b></li>
+                <li> <b>Number of Guests : ${bookingData.guests} </b> </li>
+                <li> <b>Location: <a href=https://maps.google.com/?q=${bookingData.lat},${bookingData.long}>Click Here</a></b></li>
+            </ul> 
+            
             ` // html body
 });
